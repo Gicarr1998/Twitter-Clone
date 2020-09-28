340e7d4bd8d1a705ac7a9053872d05932f3b668b
Gicarr1998 <rgica98@gmail.com>
2020-09-28 21:56:51 +0800

    First commit

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  has_secure_password
+  #Basic validation
+  validates :name, presence: true, length: {minimum:5, maximum:50}
+  #Validate email
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { minimum:5, maximum:255 },
+                                    format: { with: EMAIL_REGEX },
+                                    uniqueness: { case_sensitive: false }
+  #Validate password
+  validates :password, presence: true, length: { minimum:6 }
+end
